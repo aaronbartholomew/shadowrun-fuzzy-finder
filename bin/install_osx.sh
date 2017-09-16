706ab10481b1ecf9d6b3eeabe6d292e056612ac0
Aaron Bartholomew <aaron@bartholomew.io>
2017-09-16 15:52:08 -0700

    need to add additional python install for osx script

--- bin/install_osx.sh
+++ bin/install_osx.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-if brew ls --versions python3 > /dev/null; then
-	echo "installed"
+if hash brew 2>/dev/null; then
+		echo "homebrew installed"
 else
+	echo "installing homebrew"
 		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if brew ls --versions python3 > /dev/null; then
+	echo "python3 installed"
+else
+	echo "installing python3"
 		brew install python3
 fi
+
 pip install virtualenv
 virtualenv venv --python=/usr/local/bin/python3
 source venv/bin/activate
